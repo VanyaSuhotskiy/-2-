a39af7f390921554946d29b9c9db1374b16571ff
VanyaSuhotskiy <71978876+VanyaSuhotskiy@users.noreply.github.com>
2020-10-28 11:36:46 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,45 +1,62 @@
 ﻿using System;
 
-namespace лб_2._2
+namespace лб2_._1
 {
-
-    class Book
+    public class Point
     {
-        public Title title = new Title();
-        public Author author = new Author();
-        public Content content = new Content();
-    }
-
-    class Title
-    {
-        public string title = "Мёртвые души";
-        public void Show()
+        public int x { get; }
+        public int y { get; }
+        public string text { get; }
+        public Point(int x, int y)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(title);
-            Console.ForegroundColor = ConsoleColor.White;
+            this.x = x;
+            this.y = y;
         }
     }
 
-    class Author
+    class Figure
     {
-        public string author = "Николай Гоголь";
-        public void Show()
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(author);
-            Console.ForegroundColor = ConsoleColor.White;
-        }
-    }
+        public double Perimeter;
+        public string Name { get; }
 
-    class Content
-    {
-        public string content = "content";
-        public void Show()
+        public Figure(Point p1, Point p2, Point p3)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(content);
-            Console.ForegroundColor = ConsoleColor.White;
+            Name = "Треугольник";
+            PerimeterCalculator(p1, p2, p3);
+        }
+
+        public Figure(Point p1, Point p2, Point p3, Point p4)
+        {
+            Name = "Четырёхугольник";
+            PerimeterCalculator(p1, p2, p3, p4);
+        }
+
+        public Figure(Point p1, Point p2, Point p3, Point p4, Point p5)
+        {
+            Name = "Пятиугольник";
+            PerimeterCalculator(p1, p2, p3, p4, p5);
+        }
+
+        public double LengthSide(Point A, Point B)
+        {
+            int l = Math.Abs(B.x - A.x);
+            int h = Math.Abs(B.y - A.y);
+            return Math.Sqrt(h * h + l * l);
+        }
+
+        void PerimeterCalculator(Point p1, Point p2, Point p3)
+        {
+            Perimeter = LengthSide(p1, p2) + LengthSide(p2, p3) + LengthSide(p3, p1);
+        }
+
+        void PerimeterCalculator(Point p1, Point p2, Point p3, Point p4)
+        {
+            Perimeter = LengthSide(p1, p2) + LengthSide(p2, p3) + LengthSide(p3, p4) + LengthSide(p4, p1);
+        }
+
+        void PerimeterCalculator(Point p1, Point p2, Point p3, Point p4, Point p5)
+        {
+            Perimeter = LengthSide(p1, p2) + LengthSide(p2, p3) + LengthSide(p3, p4) + LengthSide(p4, p5) + LengthSide(p5, p1);
         }
     }
 
@@ -47,10 +64,12 @@ namespace лб_2._2
     {
         static void Main(string[] args)
         {
-            Book book = new Book();
-            book.title.Show();
-            book.author.Show();
-            book.content.Show();
+            Point p1 = new Point(0, 0);
+            Point p2 = new Point(1, 1);
+            Point p3 = new Point(0, 4);
+            Figure figure = new Figure(p1, p2, p3);
+            Console.WriteLine("Фигура: " + figure.Name);
+            Console.WriteLine("P = " + figure.Perimeter);
             Console.ReadKey();
         }
     }
